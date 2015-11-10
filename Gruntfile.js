bf74410d8c97442deae2bf0539845dd782f5a6aa
Kaleem Clarkson <kaleemclarkson@gmail.com>
2015-11-10 15:39:16 -0500

    addition of zurb and initial setup files

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          sourceMap: true,
+        },
+        files: {
+          'css/cojaxx.css': 'scss/cojaxx.scss'
+        }
+      }
+    },
+
+    watch: {
+      grunt: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js']
+      },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+}
